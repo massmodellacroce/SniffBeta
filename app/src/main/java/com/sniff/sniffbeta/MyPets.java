4c84bb5d25f0132f106d974265d0c830e9904a8a
AdeleGaley <adelegaley@mbp-de-adele.lan>
2015-03-29 11:03:43 -0300

    Adding picture

--- app/src/main/java/com/sniff/sniffbeta/MyPets.java
+++ app/src/main/java/com/sniff/sniffbeta/MyPets.java
@@ -1,20 +1,30 @@
 package com.sniff.sniffbeta;
 
 import android.content.Intent;
+import android.media.Image;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
+import android.widget.ImageView;
+
+import java.io.IOException;
 
 
 public class MyPets extends ActionBarActivity {
+
+    ImageView imageViewPet3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_pets);
+
+        imageViewPet3=(ImageView) findViewById(R.id.imagePet1);
     }
 
     public void Cancel (View view){
@@ -23,9 +33,24 @@ public class MyPets extends ActionBarActivity {
     }
 
     public void addMyPet (View view){
-        Intent intent = new Intent(this,RegisterLostPet.class);
-        intent.putExtra("action","MyPet");
-        startActivity(intent);
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Pet Image"), 1);
+    }
+
+    public void onActivityResult(int reqCode, int resCode, Intent data) {
+        if (resCode == RESULT_OK){
+            if (reqCode == 1)
+                try {
+                    imageViewPet3.setImageBitmap(MediaStore.Images.Media.getBitmap(
+                            this.getContentResolver(), data.getData()));
+                    Log.d("MyPets","Setting Image");
+                } catch (IOException e) {
+                    Toast.makeText(getApplicationContext(), "Couldn't save image",Toast.LENGTH_LONG);
+                    e.printStackTrace();
+                }
+        }
     }
 
     public void save (View view){
